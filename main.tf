2b1ef0ca7e66b08f3f746698648cb9bf1e35a2c3
Juan Sanchez <juan.sanchez1192@gmail.com>
2018-10-31 13:29:25 +0100

    Add dev service redeployment strategy without downtime

--- main.tf
+++ main.tf
@@ -248,7 +248,7 @@ resource "aws_ecs_service" "this" {
   depends_on = ["aws_lb_target_group.this", "aws_lb_listener.this"]
 }
 
-resource "aws_ecs_service" "this_dev" {
+resource "aws_ecs_service" "this_devlopment" {
   count = "${var.development_mode && length(var.services) > 0 ? length(var.services) : 0}"
 
   name            = "${element(keys(var.services), count.index)}"
@@ -257,8 +257,8 @@ resource "aws_ecs_service" "this_dev" {
   desired_count   = "1"
   launch_type     = "FARGATE"
 
-  deployment_minimum_healthy_percent = 0
-  deployment_maximum_percent         = 100
+  deployment_minimum_healthy_percent = 100
+  deployment_maximum_percent         = 200
 
   network_configuration {
     security_groups  = ["${element(aws_security_group.services.*.id, count.index)}"]
@@ -315,7 +315,7 @@ resource "aws_service_discovery_service" "development" {
   }
 
   health_check_custom_config {
-    failure_threshold = 2
+    failure_threshold = 1
   }
 }
 
